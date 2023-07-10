8f6a359a2fdb148c71983dc2af74a94b4cdad3ee
Heberluc <heberlucero48@gmail.com>
2023-07-10 17:32:08 -0300

    PreEntrega2-Lucero

--- index.js
+++ index.js
@@ -1,50 +1,79 @@
-//Algoritmo con un ciclo
-// Utilizo funciones
+const carrito = [
 
-function indicarDias() {
-    console.log( 'Todos los dias disponibles para realizarte tu servicio preferido son:')
-    for (let i=1 ; i <= 6 ; i++){
-        console.log(i + ' Dia de la semana')
+]
+
+const perfumes = [
+    {codigo:1, nombre: 'ESPARTA VICTORY', precio: 4500 },
+    {codigo:2, nombre: 'LAS VEGAS', precio: 4300 },
+    {codigo:3, nombre: 'PARÍS', precio: 4350 },
+    {codigo:4, nombre: 'DETROIT', precio: 4600 },
+    {codigo:5, nombre: 'MANHATTAN', precio: 4689 },
+    {codigo:6, nombre: 'NEW YORK HEROES', precio: 4999 },
+]
+
+class Compra { 
+    constructor(carritoDeCompras){
+        this.carrito = carritoDeCompras
     }
+    obtenerSubtotal (){
+        if (this.carrito.length > 0 ) {
+            return this.carrito.reduce((acc, perfume)=> acc + perfume.precio, 0)
+        }
+    }    
+} 
+
+function buscarPerfume(codigo) {
+        let resultado = perfumes.find((perfume)=> perfume.codigo === parseInt(codigo))
+        return resultado 
+}       
+
+function finalizarCompra() {
+    const comprar = new Compra(carrito)
+    console.log (`El valor total de su compra es de $ ${comprar.obtenerSubtotal()}. Gracias por su compra!.🤩`)
 }
 
-//Algoritmo con un condicional
-// Utilizo funciones
+function ordenarPerfumes() {
+    perfumes.sort((a, b)=>{
+        if (a.codigo > b.codigo) {
+            return 1
+        }
+        if (a.codigo < b.codigo) {
+            return -1
+        }
+        return 0
+    })
+    console.table(perfumes)
+}
 
-function indicarPrecio(codigo){
-    switch (codigo) {
-        case '1':
-            console.log('El precio del Corte de Pelo es: $ 1000')
-            break;
-        case '2':
-            console.log('El precio del Corte de Niño es: $ 1500')
-            break;
-        case '3':
-            console.log('El precio del Corte con Barba es: $ 1200')
-            break;
-        case '4':
-            console.log('El precio de Tintura Completa es: $ 5000')
-            break;   
-        case '5':
-            console.log('El precio de Mechas/Reflejos es: $ 4500')
-            break;     
-        default:
-            console.warn('No entendimos que servicio deseas real.')
-            break;
-    }
-    }
+function ordenarCarrito() {
+    carrito.sort((a, b)=>{
+        if (a.codigo > b.codigo) {
+            return 1
+        }
+        if (a.codigo < b.codigo) {
+            return -1
+        }
+        return 0
+    })
+    console.table(carrito)
+}
 
-//Simulador Interactivo
 
-function consultarServicios() {
-    let respuesta = confirm('¿Quieres saber el precio de algun servicio?')
-    if(respuesta === true){
-        let codigo = prompt('Ingresa el codigo numerico del sevicio a consultar')
-            indicarPrecio(codigo)
-            indicarDias()
+
+function comprar() {
+    let codigo = prompt('Ingresa el codigo de tu perfume elegido 👌' )
+    let perfumeElegido = buscarPerfume(codigo)
+    if(perfumeElegido !== undefined) {
+        carrito.push(perfumeElegido)
+        alert(perfumeElegido.nombre + ' se agrego a tu carrito.✅')
+        let respuesta = confirm('¿Quieres agregar otro perfume?')
+        if (respuesta === true) {
+            comprar()
+        } else {
+            ordenarCarrito()
+            finalizarCompra()
+        }
     } else{
-        console.error('La proxima te esperamos 🪒💈')
-
-    }
+        console.error('😯Error en el codigo ingresado. Vuelve a empezar su compra');
+    } 
 }
-
